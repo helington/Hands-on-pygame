b1ef6a19964fb9298c9a79b3e7f42a60a6daf7c0
Helington Willamy <willamy@Willamys-Mac.local>
2025-07-27 12:42:16 -0300

    Add: create some surfaces

--- src/runner.py
+++ src/runner.py
@@ -1,14 +1,32 @@
 import pygame
+from os import path
+
+from constants import (
+    GRAPHICS_DIR,
+    AUDIO_DIR,
+    FONT_DIR,
+    HEIGHT_SCREEN,
+    WIDTH_SCREEN,
+    FPS
+)
 
 # Initialize Pygame
 pygame.init()
 
 # Set up the screen
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((WIDTH_SCREEN, HEIGHT_SCREEN))
 pygame.display.set_caption('Runner')
 
 # Clock to control the ceil frame rate
 clock = pygame.time.Clock()
+
+# Create a surface for sky image
+sky_path = path.join(GRAPHICS_DIR, 'Sky.png')
+sky_surface = pygame.image.load(sky_path)
+
+# Crate a surface for ground image
+ground_path = path.join(GRAPHICS_DIR, 'ground.png')
+ground_path = pygame.image.load(ground_path)
 
 # Game loop
 running = True
@@ -21,10 +39,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_path, (0, sky_surface.get_height()))
+
     # Update display
     pygame.display.update()
 
     # Cap the frame rate
     clock.tick()
 
-pygame.quit()
+pygame.quit(FPS)
